2a2e595464244af7eda32b0b3b2f4d1f2a5fa6c9
rbel.io <13813090+rbel-io@users.noreply.github.com>
2023-08-02 10:18:58 +0200

    Create renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  autodiscover: false,
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  includeForks: false,
+  dryRun: 'full',
+  repositories: ['rbel-io/renovate-test-project']
+};
