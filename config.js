4057bc795c8e84dd9065e18edeedc5e06ef75539
M.Breedveld <info@rbel.io>
2023-08-20 21:00:48 +0200

    Update config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    autodiscover: false,
+    gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+    onboarding: false,
+    platform: "github",
+    forkProcessing: "disabled",
+    prHourlyLimit: 0,
+    repositories: ['rbel-io/renovate-test-project2'],
+    timezone: "Europe/Amsterdam"
+};
+
